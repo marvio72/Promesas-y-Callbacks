3952c1b8f2d6ce60b62d9a540b0df788f7827d9c
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-06-13 13:35:30 -0500

    Standard del manejo de errores con callbacks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import './sass/estilos.scss';
 
 const heroeId = 'spider';
 
-buscarHeroe( heroeId, (heroe) => {
-  console.log(heroe);
+buscarHeroe( heroeId, (err, heroe) => {
+
+  if (err) {
+    console.error(err);
+  } else {
+    console.info(heroe);
+  }
 });
+
+console.log('Finaliza el programa');
--- src/js/callbacks.js
+++ src/js/callbacks.js
@@ -15,6 +15,12 @@ const heroes = {
 
 export const buscarHeroe = (id, callback) => {
   const heroe = heroes[id];
-  callback( heroe );
 
-}
+  if (heroe) {
+    callback(null, heroe);
+  } else {
+    //Un error
+    callback(`No exitste un héroe con el id ${id}`);
+  }
+
+}; 
